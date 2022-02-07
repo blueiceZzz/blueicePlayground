d01a1615a77594224a60eb5ce55d42e7a4ba7f0f
blueiceZzz <blueicy.zzz@gmail.com>
2022-02-07 19:20:56 +0100

    configure port

--- webserver/server/src/app.ts
+++ webserver/server/src/app.ts
@@ -8,6 +8,12 @@ app.get("/", (req, res) => {
     res.send("Hallo ihr penner")
 });
 
-app.listen(3000, () => {
-    console.log("server started on localhost:3000");
+let port:any = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
+
+app.listen(port, () => {
+    console.log("server started on localhost: ", port);
 });
